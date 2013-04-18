af28a8ac994f656fd5e3eb1085cfecdce4a87d9b
David Fischer <djfische@gmail.com>
2013-04-17 18:56:59 -0700

    Always enable admin

--- src/gdc2/urls.py
+++ src/gdc2/urls.py
@@ -6,10 +6,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-        url(r'^admin/', include(admin.site.urls)),
-    )
