11761feae5694117de9d455cad78bb094e9459b1
David Fischer <djfische@gmail.com>
2013-05-04 11:01:59 -0700

    Workaround for the repo url issue in the data

--- queries/toprepos.sql
+++ queries/toprepos.sql
@@ -2,8 +2,12 @@
 .mode csv
 
 -- Gets the top 200 repos by number of forks
-SELECT DISTINCT(repository_name)
+SELECT repository_url, MAX(repository_forks) AS forks
 FROM event
 WHERE repository_fork = 0
-ORDER BY repository_forks
-DESC LIMIT 200;
+  -- Some URLs are in the database with the username
+  -- incorrectly stripped
+  AND repository_url NOT LIKE 'https://github.com//%'
+GROUP BY repository_url
+ORDER BY forks DESC
+LIMIT 200;
