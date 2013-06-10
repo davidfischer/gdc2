ec93cc61b4e45fe17792a8562f81e95f7a7b64f8
David Fischer <djfische@gmail.com>
2013-06-10 07:58:57 -0700

    Make the queries C# specific

--- queries/toprepos.sql
+++ queries/toprepos.sql
@@ -1,13 +1,14 @@
 .headers on
 .mode csv
 
--- Gets the top 200 repos by number of forks
+-- Gets the top 50 C# repos by number of forks
 SELECT repository_url, MAX(repository_forks) AS forks
 FROM event
 WHERE repository_fork = 0
   -- Some URLs are in the database with the username
   -- incorrectly stripped
   AND repository_url NOT LIKE 'https://github.com//%'
+  AND repository_language LIKE 'C#'
 GROUP BY repository_url
 ORDER BY forks DESC
-LIMIT 200;
+LIMIT 50;
